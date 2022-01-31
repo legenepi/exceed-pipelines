104eb72db3d93c2fc1f99c485e1aeef18ad3612b
Altaf Ali <7231912+altaf-ali@users.noreply.github.com>
2022-01-31 14:16:51 +0000

    adding LoadDatastoreTable step

---
+++ R/LoadDatastoreTable.R
@@ -0,0 +1,27 @@
+#' LoadDatastoreTable
+#'
+#' Load a table from datastore
+#'
+#' @docType class
+#' @format An R6 class object.
+#' @description LoadDatastoreTable class
+#' @importFrom R6 R6Class
+LoadDatastoreTable <- R6::R6Class(
+  "LoadDatastoreTable",
+  inherit = exceedapi::Step,
+
+  private = list(
+    .source = NULL,
+    .dataset = NULL
+  ),
+
+  public = list(
+    transform = function(.data, .refresh_cache = FALSE, ...) {
+      self$client$datastore(
+        source = self$args$source,
+        dataset = self$args$dataset
+      ) %>%
+        collect(.refresh_cache = .refresh_cache)
+    }
+  )
+)
