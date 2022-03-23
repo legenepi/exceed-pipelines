4546d4cc5cd6fba7a4d759ade8ebc1f79f6d2026
Altaf Ali <7231912+altaf-ali@users.noreply.github.com>
2022-03-23 16:30:45 +0000

    adding ZeroPadding step

---
+++ R/ZeroPadding.R
@@ -0,0 +1,13 @@
+ZeroPadding <- R6::R6Class(
+  "ZeroPadding",
+  inherit = exceedapi::Step,
+
+  public = list(
+
+    #' transform the data by zero-padding the exceed_id
+    transform = function(.data, ...) {
+      mutate(.data, exceed_id = str_pad(exceed_id, 6, pad = 0))
+    }
+
+  )
+)
